da713aec0dae2a9bc7eaf7a016fe74c7e2ed6ee7
Leonardo Geja <leonardo.geja@dotgroup.com.br>
2023-06-10 12:25:06 -0300

    chore: first commit

---
+++ vite.config.js
@@ -0,0 +1,43 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import * as glob from "glob";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+let sass = Object.fromEntries(
+    glob
+        .sync("resources/sass/pages/**/*.scss")
+        .map((file) => [
+            path.relative(
+                "resources/sass/pages",
+                file.slice(0, file.length - path.extname(file).length)
+            ),
+            fileURLToPath(new URL(file, import.meta.url)),
+        ])
+);
+sass = Object.values(sass);
+
+let js = Object.fromEntries(
+    glob
+        .sync("resources/js/pages/**/*.js")
+        .map((file) => [
+            path.relative(
+                "resources/js/pages",
+                file.slice(0, file.length - path.extname(file).length)
+            ),
+            fileURLToPath(new URL(file, import.meta.url)),
+        ])
+);
+js = Object.values(js);
+
+let input = [sass, js];
+input = [].concat(...input);
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input,
+            refresh: true,
+        }),
+    ],
+});
